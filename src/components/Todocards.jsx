0ddf6508a182a31799e86b892b666be316790792
khameer_13 <khameer2002@gmail.com>
2024-02-28 12:55:24 +0530

    kk

--- src/components/Todocards.jsx
+++ src/components/Todocards.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 function Todocards({ values, setValues }) {
-  const [privateTodoValues, setPrivateTodoValues] = useState([{
-    }]);
+  const [privateTodoValues, setPrivateTodoValues] = useState([]);
 
   useEffect(() => {
     const todos = JSON.parse(localStorage.getItem("todos"));
@@ -22,7 +21,7 @@ function Todocards({ values, setValues }) {
   //    prev.map((todo)=>{
   // todo.id==id ? !todoCheck : todoCheck;
   // })
-     
+
   //   ]);
   // }
 
@@ -34,11 +33,10 @@ function Todocards({ values, setValues }) {
     );
   };
 
-
   return (
     <div className="flex items-center justify-center">
       <div className="sm:w-4/5 w-4/5">
-        {privateTodoValues.map((i) => (
+        {/* {privateTodoValues.map((i) => (
           <div 
             key={i.todoID}
             className={i.todoCheck ? " w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base dark:bg-gray-700 dark:border-gray-600 dark:text-white mb-4 flex justify-content"
@@ -46,7 +44,29 @@ function Todocards({ values, setValues }) {
           >
            <input className="mr-2" type="checkbox" value={i.todoCheck}  checked={!i.todoCheck}  onChange={()=>handlecheck(i.todoID)} /> {i.todoTask}
           </div>
-        ))}
+        ))} */}
+
+        {privateTodoValues !== null &&
+          privateTodoValues.map((i) => (
+            <div
+              key={i.todoID}
+              className={
+                i.todoCheck
+                  ? "w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base dark:bg-gray-700 dark:border-gray-600 dark:text-white mb-4 flex justify-content"
+                  : "w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base dark:bg-gray-700 dark:border-gray-600 dark:text-white line-through mb-4 flex justify-content"
+              }
+            >
+              <input
+                className="mr-2"
+                type="checkbox"
+                value={i.todoCheck}
+                checked={!i.todoCheck}
+                onChange={() => handlecheck(i.todoID)}
+              />{" "}
+              {i.todoTask}
+            </div>
+          ))}
+
         <button
           className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600
            hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
